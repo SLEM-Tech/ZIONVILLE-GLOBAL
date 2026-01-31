759cd620b4c04fb1fc800cc01d1b5cae826c9274
Anselm Fowel <slemfowel@gmail.com>
2026-01-31 17:33:54 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,22 +27,23 @@ export const metadata: Metadata = {
 		],
 	},
 	other: {
-		"application/ld+json": JSON.stringify({
-			"@context": "https://schema.org",
-			"@type": "Organization",
-			"name": "Zion Ville",
-			"founder": {
-				"@type": "Organization",
-				"name": "SLEM Technologies",
-				"url": "https://slemtech.com",
-				"founder": {
-					"@type": "Person",
-					"name": "Anselm Fowel",
-					"url": "https://anselmfowel.com",
-					"jobTitle": "CEO & CTO"
-				}
-			}
-		})
+  "application/ld+json": JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "zionville-Global",
+    "creator": {
+      "@type": "Organization",
+      "name": "SLEM Technologies",
+      "url": "https://slemtech.com",
+      "founder": {
+        "@type": "Person",
+        "name": "Anselm Fowel",
+        "url": "https://anselmfowel.com",
+        "jobTitle": "CEO & CTO"
+      },
+      "role": "Paid Consultant"
+    }
+  })
 	}
 };
 
